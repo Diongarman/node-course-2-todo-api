3c099cabd5ae297d297952b12b1fc4deef9b7040
Dion Garman <diongarman@gmail.com>
2018-05-16 14:25:51 +0100

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -44,6 +44,8 @@ UserSchema.methods.toJSON = function () {
     return _.pick(userObject, ['_id', 'email']);
 };
 
+
+//Instance methods
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
@@ -53,6 +55,29 @@ UserSchema.methods.generateAuthToken = function () {
 
     return user.save().then(() => {
         return token;
+    });
+};
+
+//Model methods
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token,'abc123');
+    } catch (e) {
+
+        // return new Promise((resolve, reject) => {
+        //     reject();
+        // });
+
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
     });
 };
 
--- server/server.js
+++ server/server.js
@@ -8,8 +8,10 @@ var jwt = require('jsonwebtoken');
 
 
 var {mongoose} = require('./db/mongoose');
-var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
+
 
 var app = express();
 const port = process.env.PORT;
@@ -141,6 +143,12 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
