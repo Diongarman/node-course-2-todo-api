2acb466e864920689d01529a64fef5b963ace254
Dion Garman <diongarman@gmail.com>
2018-05-16 15:39:16 +0100

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,33 @@
-var {SHA256} = require("crypto-js");
-var jwt = require('jsonwebtoken');
+const {SHA256} = require("crypto-js");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
 
-var token = jwt.sign(data, 'abc123younme');
-console.log(token)
-var decoded = jwt.verify(token, 'abc123younme');
 
-console.log(decoded);
+var password = '123abc';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     })
+// })
+
+
+var hashedPassword = '$2a$10$EneL.qFbLn23TUHVR.iZjeI8.FDxOnFX8BaNIDYcLfKp4Wy2cUaFq';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, 'abc123younme');
+// console.log(token)
+// var decoded = jwt.verify(token, 'abc123younme');
+
+// console.log(decoded);
 
 
 
