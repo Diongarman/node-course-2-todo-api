45561e924507bf012dd23aa64ede16001406fa97
Dion Garman <diongarman@gmail.com>
2018-05-24 15:48:04 +0100

    Removed redundant config.json import

--- server/models/user.js
+++ server/models/user.js
@@ -4,8 +4,6 @@ const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 const bcrypt = require('bcryptjs');
 
-const config = require('./../config/config.json');
-
 var UserSchema = new mongoose.Schema({
     email: {
         type: String,
