abf0618e75ff9e46b7b2c5f6f2bca1112bfb7849
Dion Garman <diongarman@gmail.com>
2020-03-01 14:32:05 +0000

    configin' cors

--- server/server.js
+++ server/server.js
@@ -10,6 +10,16 @@ const bcrypt = require('bcryptjs');
 var cors = require('cors');
 
 app.options('*', cors());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  );
+  next();
+});
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
