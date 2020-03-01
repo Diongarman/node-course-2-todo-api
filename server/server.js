b513a9a9f63faa2b7f27c27e2ed878efeebb9669
Dion Garman <diongarman@gmail.com>
2020-03-01 14:51:56 +0000

    add cors

--- server/server.js
+++ server/server.js
@@ -18,15 +18,10 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+    'Origin, X-Requested-With, Content-Type, Accept,x-auth'
   );
   next();
 });
-res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
 
 const port = process.env.PORT;
 
